fd00a422eb841fb725aff73051a6c638283d8b33
David Schneider <davidschneider@Davids-MacBook-Pro.local>
2019-11-12 11:44:34 -0500

    Caesar Cipher ROT13

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,26 +1,21 @@
 
-import string
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 alphabet_index_len = len(alphabet) - 1 
-# print(alphabet)
-ciphered_list = list((input("Input ciphered text here: ")).lower())
-# print(ciphered_text)
-offset = 13
-past_range_offset = offset - 1
 
+ciphered_list = list((input("Input ciphered text here: ")).lower())
 deciphered_list = []
 
+offset = 13
+past_range_index = offset - 1
+
 for letter in ciphered_list:
-    # print(letter)
     if letter == ' ':
         deciphered_list.append(letter)
     else:
         index = alphabet.index(letter)
-        # print(index)
         shift_position = index + offset
         if shift_position > alphabet_index_len:
-            shift_position = past_range_offset - (alphabet_index_len - index)
+            shift_position = past_range_index - (alphabet_index_len - index)
         # print(shift_position)
         shifted_letter = alphabet[shift_position]
         # print(shifted_letter)
